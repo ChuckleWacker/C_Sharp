296c9127576c35952bdcc0f4a4ed9c11ae7ff470
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-07-25 23:12:44 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test WriteLine command");
+            Console.WriteLine(""); // Empty Line
+            string a = "First";
+            string b = "Last";
+            Console.WriteLine("Displaying variable A = {0} and B = {1}", a, b);
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();  //
+
+        }
+    }
+}
