0ca906aeb4549da18ffb923f2d834699113e1906
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-07-26 06:12:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color;
+            Console.WriteLine("What's your favorite color?");
+            color = Console.ReadLine();
+            Console.WriteLine("Your favorite color is {0}", color);
+
+            Console.WriteLine("");
+
+            int age;
+            Console.WriteLine("What's your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is {0}", age);
+
+            Console.ReadKey(); // Break
+        }
+    }
+}
